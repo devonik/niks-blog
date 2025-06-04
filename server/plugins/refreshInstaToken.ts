38dbadaf755230b4310c423096117bf71ae1a475
Niklas Grieger <niklas.grieger@devnik.dev>
2025-06-04 11:47:14 -0600

    update

--- server/plugins/refreshInstaToken.ts
+++ server/plugins/refreshInstaToken.ts
@@ -27,6 +27,7 @@ async function startScheduler() {
 async function refreshToken() {
   let token = await useStorage().getItem('token:insta')
   if(!token) token = useRuntimeConfig().IGAccessToken
+  
   await $fetch<IGApiRefreshTokenResponse>(
     `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${token}`,
   ).then(async (result) => {
