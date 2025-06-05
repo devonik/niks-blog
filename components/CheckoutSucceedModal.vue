42504fa1b910e533524be58bf1837db90f097ffc
Niklas Grieger <niklas.grieger@devnik.dev>
2025-06-04 22:37:17 -0600

    remove unused stuff

--- components/CheckoutSucceedModal.vue
+++ components/CheckoutSucceedModal.vue
@@ -2,7 +2,7 @@
 defineProps<{
   isModalVisible: boolean
 }>()
-const emit = defineEmits(['close'])
+defineEmits(['close'])
 </script>
 <template>
   <transition name="fade">
--- server/api/health.get.ts
+++ server/api/health.get.ts
@@ -1,4 +1,3 @@
-
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
   return 'Ok'
 })
