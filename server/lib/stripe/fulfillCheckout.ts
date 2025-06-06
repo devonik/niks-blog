4a566d16b964b315c238ccb6b5347cc6ae23d58d
Niklas Grieger <niklas.grieger@devnik.dev>
2025-06-05 19:22:48 -0600

    replace foreach with for loop to prevent async side effects

--- server/lib/stripe/fulfillCheckout.ts
+++ server/lib/stripe/fulfillCheckout.ts
@@ -24,29 +24,30 @@ export default async (sessionId: string) => {
   if (checkoutSession.payment_status !== 'paid') return
 
   const formattedProductLines: Record<string, string> = {}
-  checkoutSession.line_items?.data.forEach(async (item) => {
-    if (item.price?.product) {
-      const product = await stripe.products.retrieve(item.price.product as string)
-      if (product.metadata['product-key']) {
-        //Google maps list products
-        Object.keys(GOOGLE_MAPS_LIST_PRODUCT_KEY.galapagos).forEach((key) => {
-          if (product.metadata['product-key'] === GOOGLE_MAPS_LIST_PRODUCT_KEY.galapagos[key]) {
-            console.log(`Google maps list ${key} bought`)
-            formattedProductLines[product.name] = GOOGLE_MAPS_LIST_LINKS.galapagos[key]
-          }
-        })
-        console.log(
-          'Customer ' +
-            checkoutSession.customer_details?.email +
-            ' bought products. Will send email with links:',
-          formattedProductLines,
-        )
-        if (checkoutSession.customer_details?.email) {
-          await sendMail(
-            checkoutSession.customer_details.email,
-            'Your purchase was successful!',
-            '',
-            `
+  if (!checkoutSession.line_items) return
+
+  for (const lineItemData of checkoutSession.line_items.data) {
+    const product = await stripe.products.retrieve(lineItemData.price?.product as string)
+    if (product.metadata['product-key']) {
+      //Google maps list products
+      Object.keys(GOOGLE_MAPS_LIST_PRODUCT_KEY.galapagos).forEach((key) => {
+        if (product.metadata['product-key'] === GOOGLE_MAPS_LIST_PRODUCT_KEY.galapagos[key]) {
+          console.log(`Google maps list ${key} bought`)
+          formattedProductLines[product.name] = GOOGLE_MAPS_LIST_LINKS.galapagos[key]
+        }
+      })
+      console.log(
+        'Customer ' +
+          checkoutSession.customer_details?.email +
+          ' bought products. Will send email with links:',
+        formattedProductLines,
+      )
+      if (checkoutSession.customer_details?.email) {
+        await sendMail(
+          checkoutSession.customer_details.email,
+          'Your purchase was successful!',
+          '',
+          `
             <h3>Thank you for your purchase! Here are the details of your order:</h3>
             <ul>
               ${Object.entries(formattedProductLines)
@@ -54,11 +55,10 @@ export default async (sessionId: string) => {
                 .join('')}
             </ul>
           `,
-          )
-        } else throw new Error('No customer email found in checkout session')
-      } else {
-        throw new Error('Unknown product key:' + product.metadata['product-key'])
-      }
+        )
+      } else throw new Error('No customer email found in checkout session')
+    } else {
+      throw new Error('Unknown product key:' + product.metadata['product-key'])
     }
-  })
+  }
 }
--- server/lib/stripe/initStripe.ts
+++ server/lib/stripe/initStripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
-let stripe: unknown
-export default async () => {
+let stripe: Stripe
+export default async (): Promise<Stripe> => {
   if (!stripe) {
     stripe = new Stripe(useRuntimeConfig().stripeApiKey)
     if (!stripe) throw new Error('Stripe not initialized')
