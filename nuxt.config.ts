5276ecc03f3d432e6464637e2d21f906996677c5
Niklas Grieger <niklas.grieger@devnik.dev>
2025-06-09 10:17:10 -0500

    using supabase credentials instead of url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,7 +77,10 @@ export default defineNuxtConfig({
       default: {
         connector: 'postgresql',
         options: {
-          url: process.env.NUXT_DATABASE_URL || '',
+          user: process.env.POSTGRES_USER || '',
+          password: process.env.POSTGRES_PASSWORD || '',
+          host: process.env.POSTGRES_HOST || '',
+          database: process.env.POSTGRES_DATABASE || '',
         },
       },
     },
--- server/plugins/initDatabase.ts
+++ server/plugins/initDatabase.ts
@@ -1,8 +1,6 @@
 export default defineNitroPlugin(async () => {
   if (process.env.APP_ENV === 'build') {
-    console.log(
-      '[server/plugins/refreshInstaToken.ts] Skipping starting scheduler, in build context',
-    )
+    console.log('[server/plugins/initDatabase.ts] Skipping init database, in build context')
     return
   }
   const db = useDatabase()
