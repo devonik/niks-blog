566435d45e86c9132ccc0adecc2bdcb3e882cb88
Niklas Grieger <niklas.grieger@devnik.dev>
2025-06-10 16:12:03 -0500

    content: add media discaimer with affiliate link for dji osmo action 5

---
+++ components/blog/MediaDisclaimer.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <img
+      class="not-prose mb-3"
+      src="/nik.diver_qr.png"
+      heigth="200"
+      width="150"
+      alt="Instagram ni.diver QR code"
+    />
+    <div class="text-xs text-gray-600">
+      <span
+        >This content comes from my Instagram account, @nik.diver. Follow me to support me.</span
+      >
+      <span
+        >I am using the DJI osmo action 5 and it's very good for underwater environments. The white
+        balance is one of the best and no red filter needed.</span
+      >
+      <span class="text-bold"
+        >If you wanna buy the same actions cam please use this
+        <a
+          href="https://amzn.to/4jNL041"
+          alt="If you wanna buy the same actions cam please use this link to buy to support me. No extra costs for you"
+          >link</a
+        >
+        to buy to support me. No extra costs for you</span
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
